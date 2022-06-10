8a692e99c91a98995171e96d333629d372b5333f
Mike Wright <m.wright92@icloud.com>
2022-06-10 15:52:51 -0700

    complete exception module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::API
-  include Response
-  # include Exception
-
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    json_response({ message: exception.message }, :not_found)
-  end
-
-  rescue_from ActiveRecord::RecordInvalid do |exception|
-    json_response({ message: exception.message }, :unprocessable_entity)
-  end
+  include Response  
 end
--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,4 +1,7 @@
 module Response
+  extend ActiveSupport::Concern
+  include ExceptionHandle
+
   def json_response(object, status = :ok)
     render json: object, status: status
   end
