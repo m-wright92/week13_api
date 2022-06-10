067412a0331c134862afc4a8f640539d9ed6c4f2
Mike Wright <m.wright92@icloud.com>
2022-06-10 10:32:01 -0700

    update controllers

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,12 +1,36 @@
 class SpeciesController < ApplicationController
 
   def index
-    @species = {"species": "cat"}
+    @species = Quote.all
     json_response(@species)
+  end
+
+  def show
+    @species = Quote.find(params[:id])
+    json_response(@species)
+  end
+
+  def create
+    @species = Quote.create(species_params)
+    json_response(@species)
+  end
+
+  def update
+    @species = Quote.find(params[:id])
+    @species.update(species_params)
+  end
+
+  def destroy
+    @species = Quote.find(params[:id])
+    @species.destroy
   end
 
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def species_params
+    params.permit(:species_name, :latin_name)
   end
 end
