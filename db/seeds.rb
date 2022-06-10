d7a4c7ebcead617b6e8f344c0d82a236231b5269
Mike Wright <m.wright92@icloud.com>
2022-06-10 10:13:50 -0700

    build db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  Species.destroy_all
+  Pet.destroy_all
+  
+  def self.begin
+    seed = Seed.new
+    seed.generate_pets
+  end
+
+  def generate_pets
+    Species.create!(
+      species_name: "cat",
+      latin_name: "Felis catus"
+    )
+
+    Species.create!(
+      species_name: "dog",
+      latin_name: "Canis lupus familiaris"
+    )
+
+    30.times do |i|
+      pet = Pet.create!(
+        name: Faker::Artist.name,
+        klass: Faker::Games::DnD.klass,
+        level: Faker::Number.within(range: 1..20),
+        quote: Faker::Movie.quote,
+        species_id: rand(1..2)
+      )
+      puts "Pet #{i} is #{pet.name} the #{Species.find(pet.species_id).species_name}"
+    end
+  end
+end
+
+Seed.begin
