c1153117987e69ac0c6be6d1e34b0a4780200ddc
Mike Wright <m.wright92@icloud.com>
2022-06-10 16:38:32 -0700

    add pet factory

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory(:pet) do
+    name {Faker::Artist.name}
+    klass {Faker::Games::DnD.klass}
+    level {Faker::Number.within(range: 1..20)}
+    quote {Faker::Movie.quote}
+    species_id {rand(1..2)}
+  end
+end
