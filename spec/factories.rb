5e30a610050225ac498e9e3d9f2031a1b0051e26
Mike Wright <m.wright92@icloud.com>
2022-06-10 16:45:13 -0700

    update factory for scaleability

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     klass {Faker::Games::DnD.klass}
     level {Faker::Number.within(range: 1..20)}
     quote {Faker::Movie.quote}
-    species_id {rand(1..2)}
+    species_id {rand(Species.ids.first..Species.ids.last)}
   end
 end
