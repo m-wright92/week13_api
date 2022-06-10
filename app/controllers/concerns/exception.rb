840942b63dee4505701dd49267354d4c4e08ab29
Mike Wright <m.wright92@icloud.com>
2022-06-10 14:08:40 -0700

    WIP build exception handling into module

---
+++ app/controllers/concerns/exception.rb
@@ -0,0 +1,13 @@
+require 'active_support/concern'
+
+module Exception
+  extend ActiveSupport::Concern
+  
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    json_response({ message: exception.message }, :not_found)
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    json_response({ message: exception.message }, :unprocessable_entity)
+  end
+end
--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,4 +1,9 @@
+require 'active_support/concern'
 module Response
+  extend ActiveSupport::Concern
+  included do
+    self.
+  end
   def json_response(object, status = :ok)
     render json: object, status: status
   end
