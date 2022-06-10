e5e91f1fb0ab781b0adef9e4e9b36d79bc1742b8
Mike Wright <m.wright92@icloud.com>
2022-06-10 11:59:12 -0700

    add validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :species
+  validates :name, presence: true
+  validates :klass, presence: true
+  validates :level, presence: true
+  validates :level, numericality: {in: 1..20}
+  validates :species, presence: true
 end
--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,6 @@
 class Species < ApplicationRecord
   has_many :pets
+  accepts_nested_attributes_for :pets
+  validates :species_name, presence: true
+  validates :latin_name, presence: true
 end
