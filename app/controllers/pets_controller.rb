3f74c0177d65fa78adb2560652e5c295e59154a3
Mike Wright <m.wright92@icloud.com>
2022-06-10 16:09:36 -0700

    update controllers for clarity

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,27 +2,39 @@ class PetsController < ApplicationController
 
   def index
     @pets = Pet.all
-    json_response(@pets)
+    json_response(@pets, :ok)
   end
 
   def show
+    @species = Species.find(params[:species_id])
     @pet = Pet.find(params[:id])
-    json_response(@pet)
+    json_response(@pet, :ok)
   end
 
   def create
-    @pet = Pet.create!(pet_params)
-    json_response(@pet)
+    @species = Species.find(params[:species_id])
+    @pet = Species.pet.create!(pet_params)
+    json_response(@pet, :created)
   end
 
   def update
+    @species = Species.find(params[:species_id])
     @pet = Pet.find(params[:id])
-    @pet.update!(pet_params)
+    if @pet.update!(pet_params)
+      render status: 200, json: {
+        message: "#{@pet.name} has been successfully updated."
+      }
+    end
   end
 
   def destroy
+    @species = Species.find(params[:species_id])
     @pet = Pet.find(params[:id])
-    @pet.destroy
+    if @pet.destroy
+      render status: 200, json: {
+        message: "#{@pet.name} has been successfully removed."
+      }
+    end
   end
 
   private
--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -2,27 +2,35 @@ class SpeciesController < ApplicationController
 
   def index
     @species = Species.all
-    json_response(@species)
+    json_response(@species, :ok)
   end
 
   def show
     @species = Species.find(params[:id])
-    json_response(@species)
+    json_response(@species, :ok)
   end
 
   def create
     @species = Species.create!(species_params)
-    json_response(@species)
+    json_response(@species, :created)
   end
 
   def update
     @species = Species.find(params[:id])
-    @species.update!(species_params)
+    if @species.update!(species_params)
+      render status: 200, json: {
+        message: "#{@species.name} has been successfully updated."
+      }
+    end
   end
 
   def destroy
     @species = Species.find(params[:id])
-    @species.destroy
+    if @species.destroy
+      render status: 200, json: {
+        message: "#{@species.name} has been successfully removed."
+      }
+    end
   end
 
   private
