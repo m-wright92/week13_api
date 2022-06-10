d7cde40d476a038a666d417ec65ce413ccca2b3b
Mike Wright <m.wright92@icloud.com>
2022-06-10 15:07:33 -0700

    scrap exception module

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,9 +1,4 @@
-require 'active_support/concern'
 module Response
-  extend ActiveSupport::Concern
-  included do
-    self.
-  end
   def json_response(object, status = :ok)
     render json: object, status: status
   end
